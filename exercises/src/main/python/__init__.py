1a9310bfda837f7a468caae1a533d1ee52b8b038
Carlos Ambrona <cambronap@vectoritcgroup.com>
2020-10-05 16:26:48 +0200

    Ejercicio 2.6

--- exercises/src/main/python/__init__.py
+++ exercises/src/main/python/__init__.py
@@ -5,7 +5,7 @@ class Persona:
             self.nombre = nombre #self.nombre contiene el valor que asignamos al crear la instancia al atributo nombre. Siempre estará accesible al acceder a los atributos de la instancia
             self.edad = edad
 
-          def presentation(self):
+        def presentation(self):
                     print(f"Hola! Soy {self.nombre} y tengo {self.edad} años")
 
 class Trabajador(Persona):
@@ -18,9 +18,13 @@ class Trabajador(Persona):
             print(f"Hola! Soy {self.nombre} y tengo {self.edad} años")
             print(f"Mi departamento es {self.departamento} y mi puesto es {self.puesto}")
 
-trabajador_1 = Trabajador("Alberto", 22, "RRHH", "Becario")
+trabajador_1 = Trabajador("Alberto", '22', "RRHH", "Becario")
 trabajador_1.presentation()
 
 my_var_list = ['Andrea', '42', 'Ventas', 'Manager']
 trabajador_2 = Trabajador(my_var_list[0], my_var_list[1], my_var_list[2], my_var_list[3])
 trabajador_2.presentation()
+
+my_var_dict = {'nombre' : 'Andrea', 'edad' : '42', 'departamento': 'Ventas', 'puesto' : 'Manager'}
+trabajador_3 = Trabajador(my_var_dict['nombre'], my_var_dict['edad'], my_var_dict['departamento'], my_var_dict['puesto'])
+trabajador_3.presentation()
